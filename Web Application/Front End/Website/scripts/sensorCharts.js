7a6b221d6494d8c88c01eb6e2231de3dc6334ac1
NevilleElieh <81533133+NevilleElieh@users.noreply.github.com>
2023-11-21 10:59:14 -0800

    UI changes

--- Web Application/Front End/Website/scripts/sensorCharts.js
+++ Web Application/Front End/Website/scripts/sensorCharts.js
@@ -3,12 +3,20 @@ const humidityCanvas = document.getElementById("humidity-canvas").getContext("2d
 const temperatureCanvas = document.getElementById("temperature-canvas");
 const pressureCanvas = document.getElementById("pressure-canvas");
 
-const dummyData = [
+/*const dummyData = [
     [1698627883.1651702,36,70,546,4932],
     [1698627883.1651702,46,52,596,7155],
     [1698627883.1651702,17,59,588,5955],
     [1698627883.1651702,6,66,710,7351],
     [1698627883.1651702,16,7,522,7096]
+]*/
+
+const dummyData = [
+    [1, 36, 70, 546, 4932],
+    [2, 46, 52, 596, 7155],
+    [3, 17, 59, 588, 5955],
+    [4, 6, 66, 710, 7351],
+    [5, 16, 7, 522, 7096]
 ]
 
 const getTimeLabels = () => {
@@ -41,6 +49,16 @@ const getHumidityData = () => {
     return data;
 }
 
+const getPressureData = () => {
+    data = [];
+
+    dummyData.forEach(element => {
+        data.push(element[1])
+    })
+
+    return data;
+}
+
 var humidityChart = 
   new Chart(humidityCanvas, {
     type: 'line',
@@ -56,6 +74,8 @@ var humidityChart =
         }]
     },
     options: {
+        maintainAspectRatio:false,
+        responsive:true,
         legend: {
             display: false
         },
@@ -75,12 +95,70 @@ var tempChart =
         data: {
             labels: getTimeLabels(),
             datasets: [{
-                label: 'Temprature',
+                label: 'Temperature',
                 data: getTempData(),
                 fill: false,
                 borderColor: 'rgb(0, 0, 0)',
                 backgroundColor: 'rgb(245,245,245)',
                 tension: 0.1
             }]
+        },
+        options: {
+            maintainAspectRatio:false,
+            responsive:true,
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        display: false
+                    }
+                }]
+            }
         }
 });
+
+var presChart = 
+    new Chart(pressureCanvas, {
+        type: 'line',
+        data: {
+            labels: getTimeLabels(),
+            datasets: [{
+                label: 'Pressure',
+                data: getPressureData(),
+                fill: false,
+                borderColor: 'rgb(0, 0, 0)',
+                backgroundColor: 'rgb(245,245,245)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            maintainAspectRatio:false,
+            responsive:true,
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        display: false
+                    }
+                }]
+            }
+        }
+});
+
+
+const emergencyStop = document.getElementById("Emergency-Stop");
+const homeRobot = document.getElementById("Home-Robot");
+
+// Add a click event listener to make it clickable
+emergencyStop.addEventListener("click", function() {
+    alert("You clicked EMERGENCY STOP. Robot stopping now!");
+});
+
+homeRobot.addEventListener("click", function() {
+    alert("You clicked Home Robot! Robot moving into Home position.");
+});
+    
